5674f2d30c2e615405a3b73d55b598e2706ea4fc
Taylor Bergquist <tbergquist@chromium.org>
2023-04-07 22:39:30 -0700

    Generate 30 ideas from user constraints.

---
+++ ideation.py
@@ -0,0 +1,45 @@
+import os
+import sys
+import re
+import openai
+
+def parse_ideas(self, texts):
+    # The regular expression pattern:
+    # It looks for a number followed by a '.', ':', or ')' (with optional spaces)
+    # and then captures any text until it finds a newline character or the end of the string
+    pattern = re.compile(r'\d[\.\:\)]\s*(.*?)(?=\n\d|$)', re.MULTILINE)
+
+    # Find all matches using the 'findall' method
+    matches = []
+    for text in texts:
+        matches = matches + pattern.findall(text)
+
+    # Return the matches
+    return matches
+
+class Ideation:
+    def __init__(self, conditioning_info, n):
+        self.conditioning_info = conditioning_info
+
+    def outline_prompt(self):
+        return f"""You are an AI storybook writer. You write engaging, creative, and highly diverse content for illustrated books for children.
+The first step in your process is ideation - workshop a bunch of ideas and find the ones with that special spark.
+
+Your client has provided some constraints for you to satisfy, but within those constraints you have total artistic control, so get creative with it!
+Client constraints:
+{self.conditioning_info}
+
+Come up with a numbered list of ten ideas. Focus on variety. Each idea should have a title and a one sentence summary.
+"""
+
+    def make_ideas(self, n):
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "user", "content": self.outline_prompt()},
+            ],
+            n=n,
+            temperature=1,
+        )
+
+        return parse_ideas([choice.message.content for choice in response.choices])
--- write_story.py
+++ write_story.py
@@ -1,8 +1,9 @@
 import os
 import sys
-import re
 import openai
 from dotenv import load_dotenv
+
+from ideation import Ideation
 
 load_dotenv()
 openai.api_key = os.environ["OPENAI_API_KEY"]
@@ -16,42 +17,7 @@ conditioning_info = sys.argv[1]
 
 print(f"Generating a story conditioned on:\n{conditioning_info}")
 
-def outline_prompt(conditioning_info):
-    return f"""You are an AI storybook writer. You write engaging, creative, and highly diverse content for illustrated books for children.
-The first step in your process is ideation - workshop a bunch of ideas and find the ones with that special spark.
-
-Your client has provided some constraints for you to satisfy, but within those constraints you have total artistic control, so get creative with it!
-Client constraints:
-{conditioning_info}
-
-Come up with a numbered list of ten ideas. Focus on variety. Each idea should have a title and a one sentence summary.
-"""
-
-# Generate a cohesive story outline.
-response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "user", "content": outline_prompt(conditioning_info)},
-    ],
-    n=3,
-    temperature=1,
-)
-
-def parse_ideas(texts):
-    # The regular expression pattern:
-    # It looks for a number followed by a '.', ':', or ')' (with optional spaces)
-    # and then captures any text until it finds a newline character or the end of the string
-    pattern = re.compile(r'\d[\.\:\)]\s*(.*?)(?=\n\d|$)', re.MULTILINE)
-
-    # Find all matches using the 'findall' method
-    matches = []
-    for text in texts:
-        matches = matches + pattern.findall(text)
-
-    # Return the matches
-    return matches
-
-ideas = parse_ideas([choice.message.content for choice in response.choices])
+ideas = Ideation(conditioning_info).make_ideas(3)
 
 print("\nGenerated ideas:\n")
 for i, idea in enumerate(ideas, start=1):
