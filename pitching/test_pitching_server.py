4b80a2376e8405035bd7e0ac6ec38cb21c805824
Taylor Bergquist <tbergquist@chromium.org>
2023-05-24 20:46:09 -0700

    Basic, untested, websocket server for debugging loop.

---
+++ pitching/test_pitching_server.py
@@ -0,0 +1,22 @@
+import asyncio
+import json
+import openai
+
+import os
+from dotenv import load_dotenv
+from llmtaskgraph.app.server import WebSocketServer
+from llmtaskgraph.task import TaskGraphTask
+from llmtaskgraph.task_graph import TaskGraph
+
+from pitching.make_premise import make_premise, function_registry
+
+load_dotenv()
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
+# Create task graphs
+overall_graph = make_premise(2)
+graph_input = {"conditioning_info": "The client is a dog food company. The story should help sell their dog food by manipulating the readers."}
+
+server = WebSocketServer(overall_graph, function_registry, graph_input)
+print("Created server.")
+server.run()
